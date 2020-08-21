44586f77113b9de12bc2c1d60b04941289e8efdc
Meira-JH <jhalvesmeira@gmail.com>
2020-08-21 10:18:55 -0300

    scroll to top button adicionado

--- portfolio-web/src/pages/Portfolio/index.js
+++ portfolio-web/src/pages/Portfolio/index.js
@@ -1,25 +1,48 @@
-import React from 'react'
-import { PortfolioWrapper } from './style'
-import FirstBlock from '../../containers/FirstBlock'
-import SecondBlock from '../../containers/SecondBlock'
-import ThirdBlock from '../../containers/ThirdBlock'
-import FourthBlock from '../../containers/FourthBlock'
-import FifthBlock from '../../containers/FifthBlock'
-import Footer from '../../containers/Footer'
+import React from "react";
+import { PortfolioWrapper } from "./style";
+import FirstBlock from "../../containers/FirstBlock";
+import SecondBlock from "../../containers/SecondBlock";
+import ThirdBlock from "../../containers/ThirdBlock";
+import FourthBlock from "../../containers/FourthBlock";
+import FifthBlock from "../../containers/FifthBlock";
+import Footer from "../../containers/Footer";
+import ScrollTop from "react-scrolltop-button";
+import styled from "styled-components";
 
+const ScrollTopButtom = styled(ScrollTop)`
+&&{
+    background-color: rgb(255, 255, 255, 0.7);
+    color: rgb(9, 107, 166, 0.7);
+    border-color: rgb(9, 107, 166, 0.7);
 
-const Portfolio = (props) => {
-
-    return(
-        <PortfolioWrapper>
-            <FirstBlock/>
-            <SecondBlock/>
-            <ThirdBlock />
-            <FifthBlock />
-            <FourthBlock />
-            <Footer />
-        </PortfolioWrapper>
-    )
+    &:hover {
+    background-color: rgb(9, 107, 166, 1);
+    color: rgb(255, 255, 255, 1);
+    border-color: rgb(9, 107, 166, 1);
+  }
 }
 
-export default Portfolio
+`;
+
+const Portfolio = (props) => {
+  return (
+    <PortfolioWrapper>
+      <ScrollTopButtom
+        text="para o início"
+        distance={90}
+        breakpoint={768}
+        className="scroll-your-role"
+        speed={1200}
+        target={0}
+      />
+      <FirstBlock />
+      <SecondBlock />
+      <ThirdBlock />
+      <FifthBlock />
+      <FourthBlock />
+      <Footer />
+    </PortfolioWrapper>
+  );
+};
+
+export default Portfolio;
